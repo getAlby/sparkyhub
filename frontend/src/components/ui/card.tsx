bfe05a3ae4fb8fb02893d391d390eb7fe55bf7aa
René Aaron <rene@twentyuno.net>
2025-04-17 21:08:34 +0200

    fix: fonts

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -8,6 +8,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card"
       className={cn(
         "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-glass backdrop-blur-lg border border-white/30 rounded-lg shadow-glass",
         className
       )}
       {...props}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import './index.css'
 import App from './App.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
+
+import './index.css'
+import "./fonts.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
