ddfa4d9a1fd8a356db41cd596709f70a63118544
Roland Bewick <roland.bewick@gmail.com>
2025-04-25 18:27:24 +0700

    chore: make warning banner less ugly

--- frontend/src/components/WarningBanner.tsx
+++ frontend/src/components/WarningBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const WarningBanner: React.FC = () => {
   return (
     <div
-      className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-2 w-full mb-4 flex items-center justify-center gap-2 text-sm"
+      className="text-yellow-400 p-2 w-full mb-4 flex items-center justify-center gap-2 text-xs border border-t-0 border-l-0 border-r-0 bg-glass backdrop-blur-lg rounded-lg shadow-glass"
       role="alert"
     >
       <p className="font-bold">
@@ -12,9 +12,9 @@ const WarningBanner: React.FC = () => {
           href="https://github.com/getAlby/sparkyhub?tab=readme-ov-file#sparky-hub"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-yellow-700 underline hover:text-yellow-900"
+          className="text-yellow-400 underline hover:text-yellow-500"
         >
-          Learn More
+          Learn more
         </a>
       </p>
     </div>
